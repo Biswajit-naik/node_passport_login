52baae329b8570f7dfc238992bc6e9573ef6314e
BISWAJIT NAIK <naik3797@gmail.com>
2023-04-05 14:43:15 +0530

    js pushed

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,8 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+// dbPassword = 'mongodb+srv://:BN-TEST'+ encodeURIComponent('1999') + 'TESTDB.mongodb.net/test?retryWrites=true';
+const mongoose = require('mongoose');
 
+
+dbPassword=mongoURI =  `mongodb+srv://${username}:${password}@cluster0.rtqpqob.mongodb.net/dbname?retryWrites=true&w=majority` 
 module.exports = {
     mongoURI: dbPassword
 };
